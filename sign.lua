c717ca3070383295f8cdf67c701aa81066af3a3e
sharafzaman <sharafzaz121@gmail.com>
2018-11-20 02:16:32 +0530

    added verify method

--- sign.lua
+++ sign.lua
@@ -1,9 +1,9 @@
 -- read decrypted private key file
 local f = io.open("private_key", "r")
 local sign_priv = f:read()
-
-local torchbear_bin = fs.read_file(os.getenv("PROJECT")) 
 local priv_key = crypto.sign.load_secret(sign_priv)
+
+local torchbear_bin = fs.read_file(os.getenv("PROJECT"))
 
 -- generates signature
 local signature = priv_key:sign_detached(torchbear_bin)
@@ -13,3 +13,5 @@ local sig_file_name = "torchbear-" .. os.getenv("TRAVIS_TAG") .."-".. os.getenv(
 
 local file = io.open(sig_file_name, "w")
 file:write(signature, "\n")
+
+os.exit()
---
+++ verify.lua
@@ -0,0 +1,20 @@
+-- load public key
+local f = io.open("public_key", "r")
+local pub_key_string = f:read()
+local pub_key = crypto.sign.load_public(tostring(pub_key_string))
+
+-- load signature
+f = io.open("torchbear.sig", "r")
+local signature = tostring(f:read())
+
+local torchbear_bin = fs.read_file("torchbear") 
+
+local is_valid = pub_key:verify_detached(torchbear_bin, signature)
+
+if is_valid then
+	print("VERIFIED! The signature file is valid\n")
+else
+	print("The signature file is invalid\n")
+end
+
+os.exit()
