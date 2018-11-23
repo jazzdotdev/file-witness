668f1db7e85e09c95a1c4dcb893d8fce41e6973b
sharafzaman <sharafzaz121@gmail.com>
2018-11-24 02:45:29 +0530

    support for local signing

--- sign.lua
+++ sign.lua
@@ -18,10 +18,20 @@ local bin_file_content = fs.read_file(bin_file_name)
 -- generates signature
 local signature = priv_key:sign_detached(bin_file_content)
 
-local sig_file_name = bin_file_name .. os.getenv("TRAVIS_TAG") .."-".. os.getenv("ARCH")
-.."-".. os.getenv("PLATFORM") .."-".. os.getenv("CHANNEL")..".sig"
+-- Name of the generated signature file
+local sig_file_name = ""
+
+-- check if the file is running on travis or local machine
+if os.getenv("TRAVIS_TAG") ~= nil then
+	sig_file_name = bin_file_name .. os.getenv("TRAVIS_TAG") .."-".. os.getenv("ARCH")
+		.."-".. os.getenv("PLATFORM") .."-".. os.getenv("CHANNEL")..".sig"
+else
+	sig_file_name = bin_file_name .. "-local.sig"
+end
 
 local file = io.open(sig_file_name, "w")
 file:write(signature, "\n")
+
+print("Signature file: " .. sig_file_name .. " created", "\n")
 
 os.exit()
